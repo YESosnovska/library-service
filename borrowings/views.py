5aa6728ae621c54431a032ee5acdf75ed3f45ce6
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-09-14 14:56:57 +0300

    add BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+
+class BorrowingViewSet(GenericViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_queryset(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingSerializer
