6402e5ad57b6990eb300c78062a3d6af36d24ce0
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-09-14 15:18:20 +0300

    allow actual_return_date to be empty

--- borrowings/models.py
+++ borrowings/models.py
@@ -7,7 +7,7 @@ from library_service import settings
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
     book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
