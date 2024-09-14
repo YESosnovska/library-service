4ddaee823220117635817da254822dcd5184cc37
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-09-14 14:53:51 +0300

    add BorrowingListSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -20,6 +20,22 @@ class BorrowingSerializer(serializers.ModelSerializer):
         )
 
 
+class BorrowingListSerializer(serializers.ModelSerializer):
+    book_id = serializers.CharField(source="book.book_id", read_only=True)
+    user_id = serializers.CharField(source="user.user_id", read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book_id",
+            "user_id",
+        )
+
+
 class BorrowingDetailSerializer(BorrowingSerializer):
     book = BookSerializer(many=False, read_only=True)
     user_id = serializers.CharField(source="user.user_id", read_only=True)
